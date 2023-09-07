d501b9b3bfdd3d313aef4ecd706ca5e1600855a5
yusei-mizuno <143696635+yusei-mizuno@users.noreply.github.com>
2023-09-07 19:28:38 +0900

    素因数分解

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample401
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  count, amari, i;
+            amari = 0;
+            i = 1;
+            Console.Write("整数を入力：");
+    ;        int suji = int.Parse(Console.ReadLine());
+            while ( suji > 1) 
+            {
+                count = 0;
+                amari = 0;
+                i++;
+                while (amari == 0 ) 
+                {
+                    amari = suji % i;
+                    if (amari == 0) 
+                    {
+                        count++;
+                        suji = suji / i;
+                    }
+                }
+                if (count >= 1 && suji == 1) 
+                {
+                    Console.Write(i + "^" + count);
+                }
+                else if (count >= 1)
+                {
+                    Console.Write(i + "^" + count + "×");
+                }
+            }
+            Console.WriteLine("です");
+
+
+        }
+    }
+}
